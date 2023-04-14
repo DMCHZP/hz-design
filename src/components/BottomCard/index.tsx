4fab3b72e627c12e92a2643b5ef3f71b73d21499
dmchzp <1835707049@qq.com>
2023-04-14 19:09:40 +0800

    feat: bottom card

--- src/components/BottomCard/index.tsx
+++ src/components/BottomCard/index.tsx
@@ -1,8 +1,99 @@
-import React from 'react';
+import React, { useRef, useState, useLayoutEffect, useMemo } from 'react';
 import styles from './index.module.less';
 
-const BottomCard = () => {
-	return <div className={styles.container}>111</div>;
+interface BottomCardProps {
+	height: number | string;
+}
+
+type Status = 'min' | 'normal' | 'max';
+
+let moveY = 0;
+let startY = 0;
+const BottomCard = (props: BottomCardProps) => {
+	const { height } = props;
+	const [status, setStatus] = useState<Status>('min');
+	const [transform, setTransform] = useState({});
+	const cardRef = useRef<any>();
+
+	const baseNum = 1 / 6;
+
+	useLayoutEffect(() => {
+		const y = getStatusTranslateY(status);
+		setTransform({ transform: `translateY(${y}px)` });
+	}, [height]);
+
+	const onTouchStart = (e: any) => {
+		const clientY = e.touches[0].clientY;
+		startY = clientY;
+	};
+
+	const getStatusTranslateY = (status: Status): number => {
+		let val = 0;
+		if (status === 'min') {
+			val = cardRef.current.clientHeight * (2 / 3);
+		} else if (status === 'normal') {
+			val = cardRef.current.clientHeight * (1 / 3);
+		} else if (status === 'max') {
+			val = 0;
+		}
+		return val;
+	};
+
+	const onTouchMove = (e: any) => {
+		const y = e.touches[0].clientY;
+		moveY = y - startY;
+		const currentY = getStatusTranslateY(status);
+		setTransform({
+			transform: `translateY(${currentY + moveY}px)`
+		});
+	};
+
+	const onTouchEnd = (e: any) => {
+		const deltaY = e.changedTouches[0].clientY - startY;
+		const clintHeight = cardRef.current.clientHeight;
+		const currentY = getStatusTranslateY(status);
+		//max偏移量 = clientHeight*(1/6)
+		//normal偏移量 = clientHeight*(1/6)+clientHeight*(1/3)
+		const normalBase = clintHeight * baseNum + clintHeight * (1 / 3);
+		const maxBase = clintHeight * baseNum;
+		const res = currentY + deltaY;
+		if (res <= maxBase) {
+			const minY = getStatusTranslateY('max');
+			setTransform({
+				transform: `translateY(${minY}px)`
+			});
+			setStatus('max');
+		} else if (res > maxBase && res <= normalBase) {
+			const normalY = getStatusTranslateY('normal');
+			setTransform({
+				transform: `translateY(${normalY}px)`
+			});
+			setStatus('normal');
+		} else {
+			const maxY = getStatusTranslateY('min');
+			setTransform({
+				transform: `translateY(${maxY}px)`
+			});
+			setStatus('min');
+		}
+
+		moveY = 0;
+		startY = 0;
+		//
+	};
+
+	return (
+		<div
+			ref={cardRef}
+			onTouchStart={onTouchStart}
+			onTouchMove={onTouchMove}
+			onTouchEnd={onTouchEnd}
+			className={styles.card}
+			style={{ ...transform, height: height }}
+		>
+			111
+		</div>
+	);
 };
 
 export default BottomCard;
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<BottomCard />
+		<BottomCard height={800} />
 	</React.StrictMode>
 );
