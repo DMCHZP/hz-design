84ceb407fecb725300a107735a976fff3f275e58
dmchzp <1835707049@qq.com>
2023-04-14 16:21:38 +0800

    feat: init2

---
+++ src/components/BottomCard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.less';
+
+const BottomCard = () => {
+	return <div className={styles.container}>111</div>;
+};
+
+export default BottomCard;
